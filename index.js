26b5b83bf19cef024edc8a182ef809f66bb3ae4c
Charith Wickramasinghe <131009269+charith-codex@users.noreply.github.com>
2023-12-30 17:49:39 +0530

    ver 1

---
+++ index.js
@@ -0,0 +1,20 @@
+var numberOfDrumButtons = document.querySelectorAll(".drum").length;
+
+for(var i = 0; i < numberOfDrumButtons; i++){
+
+    document.querySelectorAll(".drum")[i].addEventListener("click", function (){
+        alert("clicked");
+    });
+}
+
+
+
+
+
+
+
+
+/*
+function handleClick(){
+    alert("clicked");
+}*/
